6c1338783b5cf377016fe4389a0288b0cbff3d3e
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2025-01-15 18:44:34 +0100

    feat: enhance product controller with error handling and response structure

--- src/api/controllers/product.controller.ts
+++ src/api/controllers/product.controller.ts
@@ -3,14 +3,52 @@ import { AppError } from "../middleware/error-handler";
 
 export class ProductController {
   static async createProduct(req: Request, res: Response, next: NextFunction) {
-    res.status(201).json({ message: "Product created" });
+    try {
+      res.status(201).json({
+        status: "success",
+        data: {
+          message: "createProduct",
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 
   static async restockProduct(req: Request, res: Response, next: NextFunction) {
-    res.status(200).json({ message: "Product restocked" });
+    try {
+      const { id } = req.params;
+      const { quantity } = req.body;
+
+      if (!quantity || quantity <= 0) {
+        throw new AppError(400, "Restock quantity must be greater than 0");
+      }
+      res.json({
+        status: "success",
+        data: {
+          message: "restockProduct",
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 
   static async sellProduct(req: Request, res: Response, next: NextFunction) {
-    res.status(200).json({ message: "Product sold" });
+    try {
+      const { id } = req.params;
+      const { quantity } = req.body;
+
+      if (!quantity || quantity <= 0) {
+        throw new AppError(400, "Sell quantity must be greater than 0");
+      }
+
+      res.json({
+        status: "success",
+        data: { message: "sellProduct" },
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 }
